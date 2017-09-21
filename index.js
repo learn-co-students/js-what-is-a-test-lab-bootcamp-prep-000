b350dbfc1b98ecf5a22ed0b1ce18d8eb477ab28a
Stavroula Guerrero <4stacyg@optonline.net>
2017-09-21 16:05:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // write your code below!
 var name = "Susan"
+<<<<<<< HEAD
 var height = 20
 var message = `Susan is ${height} inches tall`
+=======
+var height = "Expected {74} to be less than {40}"
+var message = ` is ${height} inches tall`
+>>>>>>> 38e8a7a2a9886806f7d3dca12edae6088c26ef56
 
 module.exports = {name, height, message}
