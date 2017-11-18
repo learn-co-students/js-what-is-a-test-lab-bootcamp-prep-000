0dfae70d2e3c55b90d6c8c985afbbf4299b304c7
Bobby Lucas <bobbylucas622@yahoo.com>
2017-11-18 03:21:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // write your code below!
-var name = "Susan"
-var height = 39
-var message = ` is ${height} inches tall`
+var name = `Susan`;
+var height = 30
+var message =` ${name} is ${height} inches tall`; 
+
+ 
+
 
 module.exports = {name, height, message}
+console.log()
--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-varr index = require("../index.js")
+var index = require("../index.js")
 
 describe('what-is-a-test', () => {
   describe('Name', () => {
