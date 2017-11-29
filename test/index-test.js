f65e4aeb094b751ca0387c704877ecf2fbd91ba4
TIRAS JORDAN <tiraspjordan@gmail.com>
2017-11-29 17:32:08 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,22 +1,22 @@
 var index = require("../index.js")
 
-describe('what-is-a-test', () => {
+describe('this-is-a-test-name', () => {
   describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+    it('returns "Joe"', () => {
+      expect(index.name).toEqual('Joe')
     })
   })
 
 
   describe('Height', () => {
-    it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
+    it('40+', () => {
+      expect(index.height).toBeGreaterThan(40)
     })
   })
 
   describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
+    it('name & height', () => {
+      expect(index.message).toExclude(index.name)
       expect(index.message).toInclude(index.height)
     })
   })
