5a29493abad955503eef45148bf1ebfcefdb5e05
Joseph C <jciv.coding@gmail.com>
2017-12-02 00:56:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-// write your code below!
-var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
-
-module.exports = {name, height, message}
+var name = "Susan"
+var height = "39"
