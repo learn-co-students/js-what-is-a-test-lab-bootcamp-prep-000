8ecd6e3ada2c5d7a99b022ce4ce3eef3a923ed33
Bonnie Brasson <bonniebrasson@yahoo.com>
2018-01-05 18:01:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // write your code below!
+console.log("Hello,World!")
 var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
+var height = "74"
+var message = `${name} is ${height} inches tall`
+ 
+ var name = "Susan"
+
+var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+console.log (message)
