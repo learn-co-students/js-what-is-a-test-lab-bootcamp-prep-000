e1f6e83d782ce688aeb67eda2e1afcbf9b61a638
vincent <omegaortiz1997@gmail.com>
2018-01-29 20:39:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // write your code below!
 var name = "Vincent"
 var height = 80
-var message = "and will be the best programmer at school"
+var message = "And will be the best programmer at school."
 var Display = `${name} is ${height} inches tall. ${message}`
 module.exports = {name, height, Display, message}
+
+
+console.log(Display)
