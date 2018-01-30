b2ef8a8274654dafc56fc7debf1363a30541a14b
Jonathan Williams <jtw365@live.missouristate.edu>
2018-01-30 01:53:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // write your code below!
-var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
+var name = "Susan"
+var height = 39
+var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+console.log(message)
