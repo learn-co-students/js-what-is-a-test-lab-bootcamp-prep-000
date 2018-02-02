4218ea46ea513934c975a91c753630cc01e20d32
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-02 16:05:28 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,22 +2,21 @@ var index = require("../index.js")
 
 describe('what-is-a-test', () => {
   describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+    it('returns "Joe"', () => {
+      expect(index.name).toEqual('Joe')
     })
   })
 
 
   describe('Height', () => {
-    it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
+    it('is less than 80', () => {
+      expect(index.height).toBeLessThan(80)
     })
   })
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
+    expect(index.message).toInclude(index.height)
     })
   })
 
