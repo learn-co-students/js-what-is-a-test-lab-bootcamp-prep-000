09102595ec23feb4bcec053cbe9840c1fe891fec
A M <anupreisha@gmail.com>
2018-02-18 18:49:06 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,7 @@ var index = require("../index.js")
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "joe"', () => {
-      expect(index.name).toEqual('Susan')
+      expect(index.name).toEqual('joe')
     })
   })
 
