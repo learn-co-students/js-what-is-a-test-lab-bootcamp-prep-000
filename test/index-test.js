7abafd3b833df224488f8a7cd0267e64105fd192
alexbman30@yahoo.com <alexbman30@yahoo.com>
2018-02-19 23:03:25 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -6,20 +6,15 @@ describe('what-is-a-test', () => {
       expect(index.name).toEqual('Susan')
     })
   })
-
-
   describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
   })
-
   describe('Message', () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
   })
-
 })
-
