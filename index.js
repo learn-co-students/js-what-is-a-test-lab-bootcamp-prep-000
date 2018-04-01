2c3a944f9eca18ac0d901a41dcfedeb6c8393656
srideviqa7@gmail.com <srideviqa7@gmail.com>
2018-04-01 05:50:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // write your code below!
-var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
+var name = "Susan"
+var height = 39
+//var message = ` ${height} to be less than 40`
+var message = `${height} to be less than 40 to include ${name}`
 
 module.exports = {name, height, message}
