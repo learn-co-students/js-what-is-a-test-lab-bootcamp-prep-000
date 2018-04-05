da4b6fc8c026bed55e202eba7b92bebc05580607
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-05 17:40:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // write your code below!
-var name = "Joe"
+var name = "Susan"
 var height = 74
+  if ("74 < 40") {
+  console.log("74 is less than 40")
+}
+if ("74 == less than 40") {
+  console.log("The answer is correct")
+} else {
+  console.log("Expected 74 < 40")
+}
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
