02d78ca33d0e0503773847366eeeb0c7f5254f0a
Hammad Ahmad <hammadmughal0@gmail.com>
2018-04-17 12:25:17 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -16,7 +16,6 @@ describe('what-is-a-test', () => {
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
   })
