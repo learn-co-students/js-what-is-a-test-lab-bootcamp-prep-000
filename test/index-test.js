ab51fb69548401cec7bc4d82d96353651ca32fe6
tpaul01@mica.edu <tpaul01@mica.edu>
2018-06-01 14:16:05 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,7 @@
 var index = require("../index.js")
 
 describe('what-is-a-test', () => {
+  
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
