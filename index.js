aa2e971d6fd345b9dc47cf2442b3291ac0fb14d2
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-05 17:33:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // write your code below!
-var name = "Joe"
+var name = "Susan"
 var height = 74
-var message = ` is ${height} inches tall`
+
+if (height > 40)
+{
+height = 39
+}
+
+var message = `${name} is ${height} inches tall`
+
+
 
 module.exports = {name, height, message}
