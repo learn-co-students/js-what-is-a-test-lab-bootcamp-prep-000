7e55940274cd59498cf65f44cd57d1df957ea71e
Kim L <kimlapan@gmail.com>
2018-06-08 10:04:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
-var height = 74
+var name = "Kim"
+var height = 40
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
