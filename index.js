337d498e4402292c97ec689ef5babf7139b288cb
Lung Chen <lungheichen@gmail.com>
2018-06-15 20:18:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = 74
-var message = ` is ${height} inches tall`
+var height = 28
+var message = ` ${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
