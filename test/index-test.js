a492ef9110bf58d5a4ab436efdc37c2b822eaad2
has_is_ere@hotmail.com <has_is_ere@hotmail.com>
2018-07-11 13:50:36 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,9 @@
 var index = require("../index.js")
 
+console.log("Name1: "+ index.name)
+console.log("Height: " + index.height)
+
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -16,6 +20,7 @@ describe('what-is-a-test', () => {
 
   describe('Message', () => {
     it('gives the name and height', () => {
+      //console.log("Name2: "+ index.name)
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
