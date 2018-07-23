28bc82f574aeeb87592aae0477c2ba61d4adeaf2
achilleas.loumis@gmail.com P. Loumis <achilleas.loumis@gmail.com>
2018-07-23 03:16:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Joe"
 var height = 74
-var message = ` is ${height} inches tall`
+var message = ` is ${74} inches tall`
 
 module.exports = {name, height, message}
