e99e68ec46a8b611a14dbe5b2e54589adcbcdab3
Hayden Sutton <haydensutton1234@gmail.com>
2018-09-11 23:10:40 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ var index = require("../index.js")
 
 describe('what-is-a-test', () => {
   describe('Name', () => {
-    it('returns "Susan"', () => {
+    it('returns "joe"', () => {
       expect(index.name).toEqual('Susan')
     })
   })
