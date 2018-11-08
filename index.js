4bb99bf45e8c8b526b0fb196df33a82189f049f5
Renae L Mitchell <oneprincesslea@gmail.com>
2018-11-08 20:20:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // write your code below!
-var name = "Susan"
-var height = 74
-var message = ` is ${height} inches tall`
+var name = "Susan";
+var height = 74;
+var message = (` is ${height} inches tall`);
+
+console.log(name + message);
+
 
 module.exports = {name, height, message}
