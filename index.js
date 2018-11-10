69a902376f68740b568404babf679c4d78a1c03a
Corinne Fox <corinnefox01@gmail.com>
2018-11-10 16:10:59 +0000

    Done.

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ var message = `is ${name}` ` is ${height} inches tall`
 
 module.exports = {name, height, message}
 
+<<<<<<< HEAD
 console.log(message)
+=======
+console.log(message) 
+>>>>>>> 73d5191b604c7b673a362c5b031efbb2235cc3df
