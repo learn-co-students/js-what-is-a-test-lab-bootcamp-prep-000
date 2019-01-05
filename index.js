a5f090169b5a3841aab56247a34f9cfeaca7ef7d
Brandon Teich <brandonteich123@gmail.com>
2019-01-05 02:04:41 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ var height = 74
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+var name = "Susan"
+var height = 39
+var message = ` ${name} is ${height} inches tall`
+module.exports = {name, height, message}
