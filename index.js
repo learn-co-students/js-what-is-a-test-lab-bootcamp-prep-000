6027a8021043221c1ca93b10e7d9976a63911caf
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-01-28 01:19:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // write your code below!
-var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
+var name = "Susan"
+var height = 34
+var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+
+/*
+describe('Name', () => {it('returns "Susan"', () => {expect(index.name).toEqual('Susan')})})
+
+describe("Height", () => {it("is less than 40", () => {expect(index.height).toBeLessThan(40)})})
+
+describe("message", () => {it("gives the name and height", () => {expect(index.message).toInclude(index.name) expect(index.message).toInclude(index.height)})})
+
+*/
