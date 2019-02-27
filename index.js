80b73ba90dc4983614f27229ebdab46b42f771f1
Nicholas Stano <nicholasstano@gmail.com>
2019-02-27 00:36:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // write your code below!
+
 var name = "Susan"
 var height = 39
-var message = `${name} is ${height} inches tall`
-https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/functions-and-scope/what-is-a-test#
+
+var message = `${name} is ${height} inches tall` 
+
 module.exports = {name, height, message}
