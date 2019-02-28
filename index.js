a51e4c4ef71d7ac3559585de82c65b7dce223777
Annia Forney <anniaforney@gmail.com>
2019-02-28 21:11:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
-
-module.exports = {name, height, message}
+var 'name' = '"Susan"'
+var height = 74;
+var message = (` is ${height} inches tall`);
+('what is a test') = ('name' and 'height')
+expect ('what is a test') = ('is ${height} inches tall')
