edf93358cec0ec1c5bab035c99909a20125e678c
Christopher Cabell <cccabell@icloud.com>
2019-03-02 00:11:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// write your code below!
-var name = "Susan"
-var height = 38
-var message = `${name} is ${height} inches tall`
+var name = "Susan";
+var height = 38;
+var message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message}
+module.exports = {name, height, message};
