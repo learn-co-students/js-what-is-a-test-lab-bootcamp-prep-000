29323673d72e7254d648585e6a8cd3b803555722
Nathan Frazer <nathan.m.frazer@gmail.com>
2019-03-28 22:29:47 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -16,7 +16,7 @@ describe('what-is-a-test', () => {
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
       expect(index.message).toInclude(index.height)
     })
   })
