d396d6b87339ccd110479385b0b263ead216a5e3
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-09 09:31:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Susan"
-var height = 68
-var message = ` is ${height} inches tall`
+var name = "Susan";
+var height = 68;
+var message = ` is ${height} inches tall`;
 
 module.exports = {name, height, message}
