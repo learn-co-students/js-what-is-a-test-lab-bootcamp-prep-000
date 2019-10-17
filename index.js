d5e605d672e76a16c97ba404add9e8bf8a637e9e
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-17 23:40:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+var index = require("../index.js");
 
 var Name = "Susan";
 var Height = "40";
 var Message = "is ${height} inches tall";
+
+// write your code below!
+var Name = "Susan";
+var Height = "40";
+var Message = ` is ${height} inches tall`;
+
 module.exports = {name, height, message};
