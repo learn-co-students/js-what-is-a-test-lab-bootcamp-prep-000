cf52dd2cc424518619596d37c3fc81c159e5246d
Andres Rivera <soulkalir@gmail.com>
2020-07-01 01:11:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // write your code below!
-var name = "Susan";
-var height = 39
-var message = ` is ${name} + ${height}inches tall`
 
-module.exports = {name, height, message}
+var name = 'Susan';
+var height = 39;
+var message = ` is ${name} + ${height}inches tall`;
+
+module.exports = {name, height, message};
